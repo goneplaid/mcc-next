1b571fe385f2edc3314d3426d5a69d3535073f1f
Shawn Wilson <wilson.ohyeah@gmail.com>
2024-02-25 14:21:05 -0800

    Simplify the avatar source scheme; use a flat map structure

--- lib/avatars/create-manifest.ts
+++ lib/avatars/create-manifest.ts
@@ -7,23 +7,26 @@ function main() {
   const judgesPath = path.join(avatarsPath, "judges");
   const contestantsPath = path.join(avatarsPath, "contestants");
 
-  const judgeImageFiles = globSync(`${judgesPath}/*.{png,jpeg,jpg}`);
-  const contestantImageFiles = globSync(
-    `${contestantsPath}/season_*/*.{png,jpeg,jpg}`
-  );
+  const avatarSources = [
+    ...globSync(`${judgesPath}/*.{png,jpeg,jpg}`),
+    ...globSync(`${contestantsPath}/season_*/*.{png,jpeg,jpg}`),
+  ];
 
-  const judgeManifest = getJudges(judgeImageFiles);
-  const contestantManifest = getContestants(contestantImageFiles);
+  const avatarManifest: Record<string, string> = {};
 
-  const avatarManifest = {
-    judges: judgeManifest,
-    contestants: contestantManifest,
-  };
+  avatarSources.forEach((avatarSrc) => {
+    const key = path.basename(avatarSrc).split(".")[0];
+    const value = path
+      .relative(__dirname, avatarSrc)
+      .replace(/\.+\/|public/g, "");
+
+    avatarManifest[key] = value;
+  });
 
   try {
     const indent = 4;
     fs.writeFileSync(
-      path.join(__dirname, "../../public/images/avatars/manifest.json"),
+      path.join(__dirname, "../../avatar_manifest.json"),
       JSON.stringify(avatarManifest, null, indent)
     );
   } catch (error) {
@@ -32,19 +35,3 @@ function main() {
 }
 
 main();
-
-function getJudges(judges: string[]) {
-  return judges.map((judge) => formatAvatarEntry(judge));
-}
-
-function getContestants(contestants: string[]) {
-  return contestants.map((contestant) => formatAvatarEntry(contestant));
-}
-
-function formatAvatarEntry(filename: string) {
-  return {
-    [path.basename(filename).split(".")[0]]: path
-      .relative(__dirname, filename)
-      .replace(/(\.+\/)+|public\//g, ""),
-  };
-}
